66c65f7bc883e96fb35c357a6c19924a668f1760
Neal Chen <Neal75418@users.noreply.github.com>
2026-01-20 19:14:34 +0800

    style: format code

--- lib/data/remote/finmind_client.dart
+++ lib/data/remote/finmind_client.dart
@@ -46,7 +46,9 @@ class FinMindClient {
   static void _validateToken(String token) {
     if (token.length < _minTokenLength) {
       // Use hardcoded value to allow const constructor
-      throw const InvalidTokenException('Token too short (minimum 20 characters)');
+      throw const InvalidTokenException(
+        'Token too short (minimum 20 characters)',
+      );
     }
     if (!_tokenPattern.hasMatch(token)) {
       throw const InvalidTokenException('Token contains invalid characters');
